bb4e1752e2ab31cc008756344d1baadc624c1bfb
Kuhn, Fabian <fabian.kuhn@zuehlke.com>
2020-09-15 09:13:45 +0200

    Add Real App

---
+++ real-app/_docs/TemplateComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const TemplateComponent = props => {
+    return (
+        <View>
+            <Text>Works</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default StartGameScreen;
---
+++ real-app/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Colors from '../constants/colors';
+
+const Header = props => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{props.title}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: "100%",
+        height: 90,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 36,
+        backgroundColor: Colors.primary
+    },
+    headerTitle: {
+        color: "black",
+        fontSize: 18
+    }
+})
+
+export default Header;
